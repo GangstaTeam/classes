93a32c617a1b0ec3534757dfbc10ea90958db859
sneakyevil <sneakyevildogs@gmail.com>
2024-02-27 00:20:14 +0100

    CameraState, WallCover

---
+++ GameObject/Character/CameraState.hh
@@ -0,0 +1,24 @@
+//  Size:           0x8
+//  Total Size:     0x38
+
+class CameraState : public GameSetObject
+{
+public:
+    enum CameraStates
+    {
+        CAMERASTATE_Navigation = 0x0,
+        CAMERASTATE_Navigation_Chainsaw,
+        CAMERASTATE_TargetLocked,
+        CAMERASTATE_MeleeTarget,
+        CAMERASTATE_FreeLook,
+        CAMERASTATE_Cal50,
+        CAMERASTATE_Sniper,
+        CAMERASTATE_Debug,
+        CAMERASTATE_Dead,
+        CAMERASTATE_FixedDirection,
+        CAMERASTATE_NumStates,
+    };
+
+    CameraStates mCameraState;     // 0x30
+    bool mTriggeredTargetLockSound;             // 0x34
+};
--- GameObject/Character/WallCover.hh
+++ GameObject/Character/WallCover.hh
@@ -1,8 +1,47 @@
-//  Size:           0xB4
-//  Total Size:     0xE4
+//  Size:           0xB8
+//  Total Size:     0xE8
 
 class WallCover : public GameSetObject
 {
 public:
+    struct WallCoverInfo
+    {
+        math::Vector point;
+        math::Vector normal;
+        bool collided;
+    };
 
+    enum WallCoverState
+    {
+        WALLCOVER_None = 0x0,
+        WALLCOVER_EnteringWallCover,
+        WALLCOVER_InCover_West,
+        WALLCOVER_InCover_East,
+        WALLCOVER_OutOfCover_East,
+        WALLCOVER_OutOfCover_East_RotatingOut,
+        WALLCOVER_OutOfCover_East_RotatingIn,
+        WALLCOVER_OutOfCover_West,
+        WALLCOVER_OutOfCover_West_RotatingOut,
+        WALLCOVER_OutOfCover_West_RotatingIn,
+        WALLCOVER_ExitingCover,
+    };
+
+    WallCoverState mWallCoverState;                         // 0x30
+    int mWallCoverInfoUpdateIndex;                          // 0x34
+    WallCoverInfo mWallCoverInfo[4];                        // 0x38
+    math::Vector mCharacterDirectionBeforeLeavingCover;     // 0xA8
+    math::Vector mCharacterPositionBeforeLeavingCover;      // 0xB4
+    float mTune_ShoulderHeight;                             // 0xC0
+    float mTune_ShoulderWidth;                              // 0xC4
+    float mTune_RaycastLengthInCover;                       // 0xC8
+    float mTune_RaycastLengthOutOfCover;                    // 0xCC
+    float mTune_DistanceToWall;                             // 0xD0
+    CharacterObject* mCharacterObject;                      // 0xD4
+    unsigned int mRotateInTimer;                            // 0xD8
+    unsigned int mRemainOutOfCoverTimer;                    // 0xDC
+    unsigned int mAimingTimer;                              // 0xE0
+    bool mIsReadyToShoot;                                   // 0xE4
+    bool mIsAiming;                                         // 0xE5
+    bool mCloseToWall;                                      // 0xE6
+    bool mInitialRayCast;                                   // 0xE7
 };
