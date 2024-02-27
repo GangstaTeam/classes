36d420a2a35fda0328b01db8c2d9ca93802adbb2
sneakyevil <sneakyevildogs@gmail.com>
2024-02-27 22:54:07 +0100

    DOFParameters, FogParameters (setup)

---
+++ Engine/Renderer/DOFParameters.hh
@@ -0,0 +1,12 @@
+//  Size:           0x18
+
+class renderer::DOFParameters
+{
+public:
+    float m_Unknown0;                       // 0x0
+    float m_Unknown1;                       // 0x4
+    float m_Unknown2;                       // 0x8
+    float m_Unknown3;                       // 0xC
+    float m_Unknown4;                       // 0x10
+    float blurPercent;                      // 0x14
+};
