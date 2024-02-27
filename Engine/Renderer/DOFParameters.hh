31ea11c7cc36eac347c670c3c778c952659a4fc8
sneakyevil <sneakyevildogs@gmail.com>
2024-02-27 23:02:23 +0100

    DOFParameters, FogParameters (member types fixes)

--- Engine/Renderer/DOFParameters.hh
+++ Engine/Renderer/DOFParameters.hh
@@ -3,10 +3,10 @@
 class renderer::DOFParameters
 {
 public:
-    float m_Unknown0;                       // 0x0
+    bool m_Unknown0;                        // 0x0
     float m_Unknown1;                       // 0x4
-    float m_Unknown2;                       // 0x8
-    float m_Unknown3;                       // 0xC
-    float m_Unknown4;                       // 0x10
+    int m_Unknown2;                         // 0x8
+    int m_Unknown3;                         // 0xC
+    int m_Unknown4;                         // 0x10
     float blurPercent;                      // 0x14
 };
--- Engine/Renderer/FogParameters.hh
+++ Engine/Renderer/FogParameters.hh
@@ -3,11 +3,11 @@
 class renderer::FogParameters
 {
 public:
-    float m_Unknown0;                       // 0x0
-    float m_Unknown1;                       // 0x4
-    float m_Unknown2;                       // 0x8
-    float m_Unknown3;                       // 0xC
-    float m_Unknown4;                       // 0x10
+    bool m_Unknown0;                        // 0x0
+    int m_Unknown1;                         // 0x4
+    int m_Unknown2;                         // 0x8
+    int m_Unknown3;                         // 0xC
+    int m_Unknown4;                         // 0x10
     float fogStart;                         // 0x14
     float fogEnd;                           // 0x18
     float m_Unknown5;                       // 0x1C
