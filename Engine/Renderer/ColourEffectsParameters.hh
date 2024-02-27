7b0b6d31e233a3b0e12540ea04edfc9f7e14c79f
sneakyevil <sneakyevildogs@gmail.com>
2024-02-27 01:37:12 +0100

    renderer::ColourEffectsParameters

---
+++ Engine/Renderer/ColourEffectsParameters.hh
@@ -0,0 +1,13 @@
+//  Size:           0x1C
+
+class renderer::ColourEffectsParameters
+{
+public:
+    float brightness;                       // 0x0
+    float contrast;                         // 0x4
+    float saturation;                       // 0x8
+    int midPoint;                           // 0xC
+    int tintRed;                            // 0x10
+    int tintGreen;                          // 0x14
+    int tintBlue;                           // 0x18
+};
