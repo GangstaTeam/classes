7b9284b4e4b92b959f1d9b7c3e4121e354c0b72f
sneakyevil <sneakyevildogs@gmail.com>
2024-02-28 16:08:46 +0100

    ScriptObject (update)

--- Engine/Object/ScriptObject.hh
+++ Engine/Object/ScriptObject.hh
@@ -4,11 +4,12 @@
 class ScriptObject : public ConsoleObject
 {
 public:
-    const char *m_KeyStr;                       // 0x8
+    const char *m_KeyStr;                       // 0x4
+    char _Pad0x8[4];                            // 0x8
     core::Key m_Key;                            // 0xC
     void *m_UnknownPtr0;                        // 0x10
     UnknownScriptObjectEntry *m_UnknownPtr1;    // 0x14
-
+    char _Pad0x18[4];                           // 0x18
     uint16_t m_ParentGroupID;                   // 0x1C
     uint16_t m_UnknownUInt1;                    // 0x20
 };
