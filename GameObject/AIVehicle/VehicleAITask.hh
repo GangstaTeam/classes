e8015ccadfb70c6d12cf59767bfb4ca22e23cac7
sneakyevil <sneakyevildogs@gmail.com>
2024-03-16 16:37:03 +0100

    VehicleAITask

---
+++ GameObject/AIVehicle/VehicleAITask.hh
@@ -0,0 +1,9 @@
+//  Size:           0x8
+//  Total Size:     0x28
+
+class VehicleAITask : public ScriptObject
+{
+public:
+    int mWeight;                // 0x20
+    int mVehicleID;             // 0x24
+};
