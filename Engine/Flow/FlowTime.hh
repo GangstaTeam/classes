2894853c7382a952c7983bb0f397dbff8e8b6f14
sneakyevil <sneakyevildogs@gmail.com>
2024-03-17 10:23:28 +0100

    FlowTime (all members)

--- Engine/Flow/FlowTime.hh
+++ Engine/Flow/FlowTime.hh
@@ -7,10 +7,10 @@ public:
     unsigned int uMilliseconds;                 // 0x4
     float fMilliseconds;                        // 0x8
     float fSeconds;                             // 0xC
-    unsigned int m_Unknown4;                    // 0x10
-    unsigned int m_Unknown5;                    // 0x14
+    unsigned int uRawMicroseconds;              // 0x10
+    unsigned int uRawMilliseconds;              // 0x14
     float fRawMilliseconds;                     // 0x18
-    unsigned int m_Unknown7;                    // 0x1C
-    unsigned int m_Unknown8;                    // 0x20
-    unsigned int m_Unknown9;                    // 0x24
+    unsigned int fRawSeconds;                   // 0x1C
+    unsigned int m_uRawMilisecondsFraction;     // 0x20
+    unsigned int m_uMilisecondsFraction;        // 0x24
 };
