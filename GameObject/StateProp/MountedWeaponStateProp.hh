9beeb419f5151a7f2374bfac27ff5e06bf81d07f
sneakyevil <sneakyevildogs@gmail.com>
2024-04-15 19:55:08 +0200

    fix miss spell

--- GameObject/StateProp/MountedWeaponStateProp.hh
+++ GameObject/StateProp/MountedWeaponStateProp.hh
@@ -6,7 +6,7 @@ class MountedWeaponStateProp : public WeaponStateProp
 public:
     bool mNPCMountable;                                      // 0x160
     ScriptObjectPointer<VehicleObject*> mVehicleObject;      // 0x164
-    ScriptObjectPointer<CharacterObject*> mNPTryingToMount;  // 0x168
+    ScriptObjectPointer<CharacterObject*> mNPCTryingToMount; // 0x168
     math::Vector mExitPosition;                              // 0x16C
     float mRotationArcDegrees;                               // 0x178
     float mHalfRotationArcRadians;                           // 0x17C
