a0033f9a1bf65f3b5ff7084f5ea562d9fc0be39d
sneakyevil <sneakyevildogs@gmail.com>
2024-04-26 19:18:04 +0200

    FEObject (last member resolved)

--- Engine/Frontend/FEObject.hh
+++ Engine/Frontend/FEObject.hh
@@ -5,5 +5,5 @@ class FEObject : public ScriptObject
 {
 public:
     pure3d::frontend::Drawable *m_pDrawable; // 0x20
-    bool m_UnknownBool_0x24;                 // 0x24
+    bool m_bUpdatingDisplayList;             // 0x24
 };
