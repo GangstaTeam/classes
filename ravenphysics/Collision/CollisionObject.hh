85e2cbeb746f1b48356fb97a4d6b0b2ffa43e45c
sneakyevil <sneakyevildogs@gmail.com>
2024-04-28 23:05:15 +0200

    can you imagine i forgot to write offset for those 2 members, because i don't remember

--- ravenphysics/Collision/CollisionObject.hh
+++ ravenphysics/Collision/CollisionObject.hh
@@ -8,7 +8,7 @@ public:
     math::Vector m_vMin;                                // 0x8
     math::Vector m_vMax;                                // 0x14
     
-    ravenphysics::CollisionVolume** m_pVolumes;         // 0x0
+    ravenphysics::CollisionVolume** m_pVolumes;         // 0x24
 
-    uint16_t m_nNumVolumes;                             // 0x0
+    uint16_t m_nNumVolumes;                             // 0x30
 };
