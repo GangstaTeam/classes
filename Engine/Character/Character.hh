5c1eac5e57d4e2acf7aa10acc956735b313923b5
sneakyevil <sneakyevildogs@gmail.com>
2024-04-30 21:42:31 +0200

    Character (some members)

--- Engine/Character/Character.hh
+++ Engine/Character/Character.hh
@@ -3,17 +3,21 @@
 class Character
 {
 public:
-    CharacterObject* m_CharacterObject;             // 0x50
-    math::Vector m_ForwardAxis;                     // 0x54
-    math::Matrix m_Transform;                       // 0x60
+    CharacterObject* m_CharacterObject;                                 // 0x50
+    math::Vector m_ForwardAxis;                                         // 0x54
+    math::Matrix m_Transform;                                           // 0x60
 
-    renderer::RenderableHandle *m_Renderable;       // 0xAC
+    renderer::RenderableHandle *m_Renderable;                           // 0xAC
  
-    CollidableHandle *m_WorldCollidable;            // 0xB8
-    CollidableHandle *m_ObjectCollidable;           // 0xBC
-    CollidableHandle *m_BulletCollidable;           // 0xC0
-
-    CharacterAnimation* m_CharacterAnimation;       // 0x178
+    CollidableHandle *m_WorldCollidable;                                // 0xB8
+    CollidableHandle *m_ObjectCollidable;                               // 0xBC
+    CollidableHandle *m_BulletCollidable;                               // 0xC0
   
-    math::Vector m_Translation;                     // 0x1A4
+    float m_HeightAdjust;                                               // 0xF8
+
+    ravenphysics::CollisionObject *m_pGroundCollisionObject;            // 0x114
+
+    CharacterAnimation* m_CharacterAnimation;                           // 0x178
+  
+    math::Vector m_Translation;                                         // 0x1A4
 };
