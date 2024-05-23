0dd92b2b9a05e5bf990c97e0d7f5700e3c33dc12
sneakyevil <sneakyevildogs@gmail.com>
2024-05-23 13:43:57 +0200

    CharacterAnimation [some members]

--- Engine/Character/Animation/CharacterAnimation.hh
+++ Engine/Character/Animation/CharacterAnimation.hh
@@ -8,7 +8,12 @@ public:
 
     math::Vector m_Velocity;                        // 0x10C
 
+    pure3d::Name m_UpperBodyStateName;              // 0x144
     pure3d::Name m_AnimationStateName;              // 0x148
+
+    int m_iStatePriority;                           // 0x154
+    int m_iRequestPriority;                         // 0x158
+    int m_iRequestID;                               // 0x15C
 
     LocoRing m_LocomotionRing;                      // 0x170
 
