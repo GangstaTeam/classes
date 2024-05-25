fcf732d10727ac45d889898da2cf4138ac5468ca
sneakyevil <sneakyevildogs@gmail.com>
2024-05-25 03:33:40 +0200

    WheeledVehiclePhysicsAttributes [setup]

---
+++ Engine/Vehicle/WheeledVehicleEngine/WheeledVehiclePhysicsAttributes.hh
@@ -0,0 +1,14 @@
+//  Size:           ?
+
+class WheeledVehiclePhysicsAttributes
+{
+public:
+    float m_fPlayerOpenDoorMaxAngle;     // 0x250
+    float m_fPlayerOpenDoorDuration;     // 0x254
+    float m_fPlayerOpenDoorFrame;        // 0x258
+    float m_fPlayerCloseDoorFrame;       // 0x25C
+    float m_fPlayerOpenDoorFrameExit;    // 0x260
+    float m_fPlayerCloseDoorFrameExit;   // 0x264
+    float m_fPlayerOpenDoorDurationExit; // 0x268
+    bool m_bNoExitingAnimation;          // 0x26C
+};
