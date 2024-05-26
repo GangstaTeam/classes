cfffa936a3c145360bed7116191039eff76eae5e
sneakyevil <sneakyevildogs@gmail.com>
2024-05-27 00:32:25 +0200

    RainTemplate [all members]

---
+++ GameObject/Template/RainTemplate.hh
@@ -0,0 +1,12 @@
+//  Size:           0x14
+//  Total Size:     0x38
+
+class RainTemplate : public Template
+{
+public:
+    char* mModelInventory;         // 0x24
+    char* mModelName;              // 0x28
+    char* mStreaksParticleSystem;  // 0x2C
+    char* mSplashesParticleSystem; // 0x30
+    char* mSoundPoiTemplateName;   // 0x34
+};
