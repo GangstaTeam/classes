b257ec0514069f49001b1552ed56ef6da565de52
sneakyevil <sneakyevildogs@gmail.com>
2024-05-27 00:47:13 +0200

    renderer::RainRenderable [setup]

---
+++ Engine/Render/Renderables/RainRenderable.hh
@@ -0,0 +1,8 @@
+//  Size:           0x3D0
+//  Total Size:     0x454
+
+class renderer::RainRenderable : public renderer::Renderable
+{
+public:
+    renderer::RainParameters m_RainParams;              // 0x84
+};
