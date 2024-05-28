c8bcddcb321dffc4e4e78e862c3f75c9f9190e84
sneakyevil <sneakyevildogs@gmail.com>
2024-05-28 17:15:40 +0200

    RDJString [amazing string class]

---
+++ Core/RDJString.hh
@@ -0,0 +1,8 @@
+//  Size:           0x24
+
+class RDJString
+{
+public:
+    char m_Data[36];            // 0x0
+    // Yes this is real, a fucking "String" with hardcoded size not even template nothing...
+};
