3add4b7ff278233caeb76bb8d3cb3f7ce65c8a6e
sneakyevil <sneakyevildogs@gmail.com>
2024-06-04 18:35:08 +0200

    GameSimObject [update]

--- Engine/Object/GameSimObject.hh
+++ Engine/Object/GameSimObject.hh
@@ -4,12 +4,12 @@
 class GameSimObject : public GameObject
 {
 public:
-    char _Pad0x74[28];                          // 0x74
+
     ScriptObject *mSpawner;                     // 0x90
-    char _Pad0x98[4];                           // 0x94
+
     uint8_t m_SpawnUsage;                       // 0x98
-    char _Pad0x99[11];                          // 0x99
-    bool m_IsInvincible;                        // 0xA4
+
+    uint8_t m_uInvincible;                      // 0xA4
     bool m_UnknownFlag0 : 1;                    // 0xA5
     bool m_Visible : 1;                         // 0xA5
     bool m_UnknownFlag2 : 1;                    // 0xA5
@@ -18,5 +18,4 @@ public:
     bool m_UnknownFlag5 : 1;                    // 0xA5
     bool m_UnknownFlag6 : 1;                    // 0xA5
     bool m_UnknownFlag7 : 1;                    // 0xA5
-    char _Pad0xA6[2];                           // 0xA6
 };
