0fbd19b678b1738525552218d79f7151f1c98bb4
sneakyevil <sneakyevildogs@gmail.com>
2024-06-04 20:31:35 +0200

    VehicleDamageTransmitProperties [done]

---
+++ GameObject/Template/Vehicle/VehicleDamageTransmitProperties.hh
@@ -0,0 +1,8 @@
+//  Size:           0x78
+//  Total Size:     0x98
+
+class VehicleDamageTransmitProperties : public ScriptObject
+{
+public:
+    VehicleZoneConnectionAttributes mAttributes;        // 0x20
+};
