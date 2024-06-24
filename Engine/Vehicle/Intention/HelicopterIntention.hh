a1eaf5f3fe6bb1781b150c2afa41fccfa64aa6b5
sneakyevil <sneakyevildogs@gmail.com>
2024-06-24 22:30:00 +0200

    HelicopterIntention [done]

---
+++ Engine/Vehicle/Intention/HelicopterIntention.hh
@@ -0,0 +1,10 @@
+//  Size:           0x10
+
+class HelicopterIntenion
+{
+public:
+    float m_fSteeringFactor;       // 0x0
+    float m_fForwardMotionFactor;  // 0x4
+    float m_fUpwardMotionFactor;   // 0x8
+    float m_fSidewaysMotionFactor; // 0xC
+};
