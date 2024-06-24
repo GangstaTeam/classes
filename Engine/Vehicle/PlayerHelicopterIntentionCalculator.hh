fd940f74625d932d180c8bc268214f55dec5f9be
sneakyevil <sneakyevildogs@gmail.com>
2024-06-24 21:57:04 +0200

    PlayerHelicopterIntentionCalculator [done]

---
+++ Engine/Vehicle/PlayerHelicopterIntentionCalculator.hh
@@ -0,0 +1,8 @@
+//  Size:           0x4
+//  Total Size:     0x10
+
+class PlayerHelicopterIntentionCalculator : public PlayerVehicleIntention
+{
+public:
+    HelicopterEngine* m_pHelicopterEngine;      // 0xC
+};
