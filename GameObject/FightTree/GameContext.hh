1780fe7929fa0dec28aae3b4615af3c612966847
sneakyevil <sneakyevildogs@gmail.com>
2024-07-02 13:43:36 +0200

    GameFightContext [done]

---
+++ GameObject/FightTree/GameContext.hh
@@ -0,0 +1,8 @@
+//  Size:           0x4
+//  Total Size:     0x20
+
+class GameFightContext : public FightContext
+{
+public:
+    ScriptObjectPointer<CharacterObject*> m_CharacterObject;    // 0x1C
+};
