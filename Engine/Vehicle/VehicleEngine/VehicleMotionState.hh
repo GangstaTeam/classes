5ee4837c2aa31817589677f4f0b56d9501cce092
sneakyevil <sneakyevildogs@gmail.com>
2024-07-05 20:12:30 +0200

    VehicleMotionState [update]

--- Engine/Vehicle/VehicleEngine/VehicleMotionState.hh
+++ Engine/Vehicle/VehicleEngine/VehicleMotionState.hh
@@ -3,6 +3,12 @@
 class VehicleMotionState
 {
 public:
-    math::Vector m_fVelocityCM;     // 0x0
-    float m_fSpeedCM;               // 0xC
+    math::Vector m_fVelocityCM;                 // 0x0
+    float m_fSpeedCM;                           // 0xC
+    math::Vector m_vecLocalAxisSpeeds;          // 0x10
+    math::Vector m_vecLocalAxisSpeedsKmh;       // 0x1C
+
+    math::Vector m_vecHeadsUpAngularSpeeds;     // 0x88
+
+    float m_fHorizontalAngleOfIncidence;        // 0x98
 };
