bd9dfa297b643a467b26a79cd38ef3048dc7e678
sneakyevil <sneakyevildogs@gmail.com>
2024-07-05 20:19:10 +0200

    WheeledVehicleEngine [update]

--- Engine/Vehicle/WheeledVehicleEngine.hh
+++ Engine/Vehicle/WheeledVehicleEngine.hh
@@ -4,4 +4,9 @@ class WheeledVehicleEngine
 {
 public:
     VehicleEngine* mVehicleEngine;                  // 0x0
+
+    WheeledVehicleIntention m_Intention;            // 0x8
+    float m_fMaxWheelTurnAngle;                     // 0x20
+
+    float m_fStoppedTimer;                          // 0x38
 };
