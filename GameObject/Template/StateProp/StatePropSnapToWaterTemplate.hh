b2d5e572df5abb38f97749a7557a626ec30be8c8
sneakyevil <sneakyevildogs@gmail.com>
2024-07-14 18:15:57 +0200

    StatePropSnapToWaterTemplate [done]

---
+++ GameObject/Template/StateProp/StatePropSnapToWaterTemplate.hh
@@ -0,0 +1,8 @@
+//  Size:           0x4
+//  Total Size:     0x28
+
+class StatePropSnapToWaterTemplate : public StatePropBehaviourTemplate
+{
+public:
+    StatePropSnapToWaterAttributes mAttributes;     // 0x24
+};
