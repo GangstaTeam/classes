53f871759a0a066844ae48dca52554cfff885904
sneakyevil <sneakyevildogs@gmail.com>
2024-07-27 17:08:49 +0200

    PauseMenuPageOptions [done]

---
+++ GameObject/Pause/PauseMenuPageOptions.hh
@@ -0,0 +1,9 @@
+//  Size:           0x8
+//  Total Size:     0x84
+
+class PauseMenuPageOptions : public PauseMenuPage
+{
+public:
+    bool mProgressiveRequest; // 0x7C
+    float mElapsedTime;       // 0x80
+};
